30122ed308eb97090381c4e80bbbf13e0a22ada5
Christopher Lone Toe <christikai2001@gmail.com>
2023-08-18 10:12:03 +0630

    fix bugs and add minimum value

--- admissionStreamlitApp.py
+++ admissionStreamlitApp.py
@@ -21,11 +21,11 @@ if name:
     elif Uni_ranking == "4":
         ranking = 4
     else:
-        g = 5
+        ranking = 5
 
-    GRE_Scores = st.number_input('Enter your Your GRE Scores: ', min_value=0, max_value=340, value=0, step=1)
-    TOFEL_Scores = st.number_input('Enter your Your TOFEL Scores: ', min_value=0, max_value=120, value=0, step=1)
-    CGPA = st.number_input('Enter your Your High School or University GPA: ', min_value=0., max_value=10., value=0., step=1., format="%.2f")
+    GRE_Scores = st.number_input('Enter your Your GRE Scores (290~340): ', min_value=290, max_value=340, value=290, step=1)
+    TOFEL_Scores = st.number_input('Enter your Your TOFEL Scores (92~120): ', min_value=92, max_value=120, value=92, step=1)
+    CGPA = st.number_input('Enter your Your High School or University GPA (6.8~10.0): ', min_value=6.8, max_value=10., value=6.8, step=1., format="%.2f")
     
     data = [ranking, GRE_Scores, TOFEL_Scores, CGPA]
     result = Titanic_LR_Model.predict([data])
